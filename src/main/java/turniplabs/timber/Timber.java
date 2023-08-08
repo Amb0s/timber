a4bdef1c6068543a09492454a493dc238b0adc0e
Amb0s <ambos@disroot.org>
2023-08-08 05:08:48 +0200

    Cleaned useless code thanks to mixins

--- src/main/java/turniplabs/timber/Timber.java
+++ src/main/java/turniplabs/timber/Timber.java
@@ -1,13 +1,8 @@
 package turniplabs.timber;
 
-import net.minecraft.client.sound.block.BlockSoundDispatcher;
-import net.minecraft.core.block.Block;
-import net.minecraft.core.block.tag.BlockTags;
-import turniplabs.halplibe.mixin.accessors.BlockAccessor;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.mixin.accessors.DispatcherAccessor;
 
 public class Timber implements ModInitializer {
     public static final String MOD_ID = "timber";
@@ -15,24 +10,6 @@ public class Timber implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Block treeBlock;
-
-        // Registers modified tree blocks
-        for (Tree tree: Tree.values()) {
-            if (!tree.equals(Tree.OAK_RETRO) && !tree.equals(Tree.CHERRY_FLOWERING) && !tree.equals(Tree.SHRUB)) {
-                Block.blocksList[tree.logID] = null;
-
-                treeBlock = new BlockTree(tree.logName,tree.logID)
-                        .withTexCoords(tree.textureCoordinates[0], tree.textureCoordinates[1],
-                                tree.textureCoordinates[2], tree.textureCoordinates[3])
-                        .withTags(BlockTags.FENCES_CONNECT, BlockTags.MINEABLE_BY_AXE);
-
-
-                ((BlockAccessor) treeBlock).callSetHardness(Tree.BARE_HANDS_HARDNESS);
-                ((DispatcherAccessor) BlockSoundDispatcher.getInstance()).callAddDispatch(treeBlock,Tree.SOUND);
-            }
-        }
-
         LOGGER.info("Timber initialized");
     }
 }
---
+++ src/main/java/turniplabs/timber/mixin/BlockLogMixin.java
@@ -0,0 +1,52 @@
+package turniplabs.timber.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLog;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.item.tool.ItemToolAxe;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import turniplabs.timber.Tree;
+
+import java.util.Arrays;
+
+@Mixin(value = BlockLog.class, remap = false)
+final class BlockLogMixin {
+    /*private static final int[] LOG_TYPES = {
+            Block.logOak.id, Block.logPine.id, Block.logBirch.id, 
+            Block.logCherry.id, Block.logEucalyptus.id, Block.logOakMossy.id
+    };*/
+
+
+    @Inject(method = "onBlockRemoval", at = @At("HEAD"), require = 0)
+    private void checkAxe(World world, int x, int y, int z, CallbackInfo ci) {
+        boolean isSneaking = Minecraft.getMinecraft(Minecraft.class).thePlayer.isSneaking();
+        ItemStack inHand = Minecraft.getMinecraft(Minecraft.class).thePlayer.getCurrentEquippedItem();
+
+        if (!isSneaking && inHand != null && inHand.getItem() instanceof ItemToolAxe) {
+            byte i = 1;
+
+            for (int j = -i; j <= i; ++j) {
+                for (int k = -i; k <= i; ++k) {
+                    for (int m = 0; m <= i; ++m) {
+                        int n = world.getBlockId(x + j, y + m, z + k);
+                        if (Arrays.stream(Tree.LOG_TYPES).anyMatch(Integer.valueOf(n)::equals)) {
+                            Block block = Block.blocksList[world.getBlockId(x + j, y + m, z + k)];
+                            int i1 = world.getBlockMetadata(x + j, y + m, z + k);
+                            if (block != null && world.setBlockWithNotify(x + j, y + m, z + k, 0)) {
+                                block.dropBlockWithCause(world, EnumDropCause.PROPER_TOOL,x + j, y + m, z + k, i1,
+                                        new TileEntity());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
