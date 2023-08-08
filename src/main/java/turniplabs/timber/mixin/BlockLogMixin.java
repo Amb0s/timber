85bb686a3e3040e610ad79a4971c08a35a72f3e6
Amb0s <ambos@disroot.org>
2023-08-08 05:54:57 +0200

    Removed the useless enum

--- src/main/java/turniplabs/timber/mixin/BlockLogMixin.java
+++ src/main/java/turniplabs/timber/mixin/BlockLogMixin.java
@@ -12,22 +12,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import turniplabs.timber.Tree;
 
 import java.util.Arrays;
 
 @Mixin(value = BlockLog.class, remap = false)
 final class BlockLogMixin {
-    /*private static final int[] LOG_TYPES = {
-            Block.logOak.id, Block.logPine.id, Block.logBirch.id, 
-            Block.logCherry.id, Block.logEucalyptus.id, Block.logOakMossy.id
-    };*/
-
-
     @Inject(method = "onBlockRemoval", at = @At("HEAD"), require = 0)
     private void checkAxe(World world, int x, int y, int z, CallbackInfo ci) {
         boolean isSneaking = Minecraft.getMinecraft(Minecraft.class).thePlayer.isSneaking();
         ItemStack inHand = Minecraft.getMinecraft(Minecraft.class).thePlayer.getCurrentEquippedItem();
+
+        int[] logIDs = {
+                Block.logOak.id, Block.logPine.id, Block.logBirch.id,
+                Block.logCherry.id, Block.logEucalyptus.id, Block.logOakMossy.id
+        };
 
         if (!isSneaking && inHand != null && inHand.getItem() instanceof ItemToolAxe) {
             byte i = 1;
@@ -36,12 +34,12 @@ final class BlockLogMixin {
                 for (int k = -i; k <= i; ++k) {
                     for (int m = 0; m <= i; ++m) {
                         int n = world.getBlockId(x + j, y + m, z + k);
-                        if (Arrays.stream(Tree.LOG_TYPES).anyMatch(Integer.valueOf(n)::equals)) {
+                        if (Arrays.stream(logIDs).anyMatch(Integer.valueOf(n)::equals)) {
                             Block block = Block.blocksList[world.getBlockId(x + j, y + m, z + k)];
                             int i1 = world.getBlockMetadata(x + j, y + m, z + k);
                             if (block != null && world.setBlockWithNotify(x + j, y + m, z + k, 0)) {
-                                block.dropBlockWithCause(world, EnumDropCause.PROPER_TOOL,x + j, y + m, z + k, i1,
-                                        new TileEntity());
+                                block.dropBlockWithCause(world, EnumDropCause.PROPER_TOOL,x + j, y + m, z + k,
+                                        i1, new TileEntity());
                             }
                         }
                     }
